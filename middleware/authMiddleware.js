706386cfe183c10866ef7a638adf9625d260a67a
HaqpaHR <pereverzev.viacheslav@gmail.com>
2023-06-15 16:43:11 +0200

    second commit

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = function (req, res, next) {
+  if (req.method === "OPTION") {
+    next();
+  }
+
+  try {
+    const token = req.headers.authorization.split(" ")[1]; //Bearer ;lakd;laksdl;asdlk
+    if (!token) {
+      res.status(401).json({ message: "Авторизуйтесь" });
+    }
+    const decoded = jwt.verify(token, process.env.SECRET_KEY_JWT);
+    req.user = decoded;
+    next()
+  } catch (e) {
+    res.status(401).json({ message: "Авторизуйтесь" });
+  }
+};
